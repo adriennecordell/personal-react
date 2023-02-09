85b2dfddd93d7ce3f36d6e89cbf93ec38b99d30a
adriennecordell <adrienne.cordellac@gmail.com>
2023-02-09 15:55:16 -0600

    added photos of projects

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,7 +1,21 @@
 import React from 'react'
+import ProjectItem from '../components/ProjectItem'
 
 function Projects() {
-    return <div>Projects</div>;
+    return (
+    <div className="projects">
+        <h1>My Projects</h1>
+        <div classname="projectList">
+            <ProjectItem  name="Musical Chairs"/>
+            <ProjectItem name="Note Taker"/>
+            <ProjectItem name=" JavaScript Quiz"/>
+            <ProjectItem name="Celebrity Astrology"/>
+            <ProjectItem name="Weather Search"/>
+            <ProjectItem name="Readme Generator"/>
+
+        </div>
+    </div>
+    );
 }
 
 export default Projects
