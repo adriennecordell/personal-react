b2574eb197f65750b78847bd8b53dbacf8e7e1c4
adriennecordell <adrienne.cordellac@gmail.com>
2023-02-09 17:49:42 -0600

    build ready

--- src/pages/ProjectDisplay.js
+++ src/pages/ProjectDisplay.js
@@ -1,16 +1,16 @@
 import React from 'react'
-import { useParams } from 'react-router-dom';
-import { ProjectList } from '../helpers/ProjectList'
+// import { useParams } from 'react-router-dom';
+// import { ProjectList } from '../helpers/ProjectList'
 import GitHubIcon from '@material-ui/icons/GitHub'
 import '../styles/ProjectsDisplay.css'
 
 function ProjectDisplay() {
-    const { id } = useParams();
-    const project = ProjectList[id];
+    // const { id } = useParams();
+    // const project = ProjectList[id];
     return (
         <div className="project">
-            <h1></h1>
-            <img/>
+            {/* <h1>{project.name}</h1> */}
+            {/* <img{project.image}/> */}
             <GitHubIcon />
         </div>
     )
